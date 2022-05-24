402e7785dae8efdbb90f044568dd092905c06eb4
jun34724 <43941511+jun34724@users.noreply.github.com>
2022-05-25 04:13:30 +0900

    AR 접근버튼 생성(테스트용) AR 내부 지도 사용자 위치 탐색 수정, 옵션 변경

--- app/src/main/java/com/example/myoungji_walk_android/AppDataBase.kt
+++ app/src/main/java/com/example/myoungji_walk_android/AppDataBase.kt
@@ -1,6 +1,8 @@
 package com.example.myoungji_walk_android
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.myoungji_walk_android.Model.BookMark
 import com.example.myoungji_walk_android.Model.History
@@ -10,9 +12,11 @@ import com.example.myoungji_walk_android.dao.HistoryDao
 @Database(entities = [History::class], version = 1)
 abstract class AppDataBase: RoomDatabase() {
     abstract fun historyDao(): HistoryDao
+
 }
 
 @Database(entities = [BookMark::class], version = 1)
 abstract class AppDataBaseBookMark: RoomDatabase() {
     abstract fun bookMarkDao(): BookMarkDao
+
 }
